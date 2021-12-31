e308cb42ab321728e8cd7786275b6b6de5df9f4f
JaydenDev <willsjayden2@gmail.com>
2021-12-31 16:02:53 -0600

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+let array = ["Banana ", "Apple ", "Cat ", "Tree ", "Acorn ", "Scratch ", "Use ", "Write ", "In ",
+"Vue ", "Svelte ", "C ", "Program ", "Write "
+]
+
+function randMSG() {
+    const word1 = array[Math.floor(Math.random() * array.length)];
+    const word2 = array[Math.floor(Math.random() * array.length)];
+    const word3 = array[Math.floor(Math.random() * array.length)];
+    const word4 = array[Math.floor(Math.random() * array.length)];
+    const word5 = array[Math.floor(Math.random() * array.length)];
+    const word6 = array[Math.floor(Math.random() * array.length)];
+    document.getElementById("span").innerHTML = (word1 + word2 + word3 + word4 + word5 + word6);
+}
