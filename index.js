2b200cc6eae70ccaae91efeba3c3d28efcdba154
JaydenDev <willsjayden2@gmail.com>
2021-12-31 19:36:56 -0600

    words

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-let array = ["Banana ", "Apple ", "Cat ", "Tree ", "Acorn ", "Scratch ", "Use ", "Write ", "In ",
-"Vue ", "Svelte ", "C ", "Program ", "Write ", "Microsoft ", "Windows ", "Mac ", "God286 ", "JaydenDev ",
+let array = ["Banana ", "Apple ", "Cat ", "Tree ", "Acorn ", "Batch ", "Use ", "Write ", "In ",
+"Vue ", "Svelte ", "C ", "Program ", "Write ", "Microsoft ", "Windows ", "Mac ", "Computer ", "Hello ",
 "Git ", "GitHub ", "Meow! ", "Who ", "What ", "Where ", "When ", "Why ", "Then ", "Bit ", "Bite ",
 "Byte ", "Kite ", "Bottom ", "Top ", "Padding ", "HTML ", "CSS ", "SCSS ", "Sometimes ", "Someday ",
-"Art ", "Math ", "Dog ", "Visual ", "Studio ", "Code "
+"Art ", "Math ", "Dog ", "Visual ", "Studio ", "Code ", "World ", "Did ", "Know ", "Random ", "Generation ",
+"Fruit ", "That ", "Generic ", "Material ", "Battle ", "Crown ", "Turn ", "Around ", "Safe ", "Sound ",
+"Pound ", "Found ", "Camera ", "Delightful ", "Frightful ", "Orb ", "Cube ", "Zoom ", "Boom ", "Cake ", "Icecream ",
+"Scream ", "Team ", "Can ", "You ", "Meat ", "Meet ", "Discrete ", "Heat ", "Dictionary ", "Pictionary ", "Array ",
+"Day ", "Say ", "May ", "June ", "August ", "Red ", "Green ", "Blue ", "Yellow ", "Software ", "Hardware ", "Tail ", "Wind ",
+"Vanilla ", "Chocolate", "Coffee ", "Randomization ", "RNG ", "Length ", "Floor ", "Door ", "More ",
+"Document ", "Get ", "Element ", "By ", "ID ", "Span ", "innerHTML ", "Though ", "Although ", "Almighty ",
+"Rust ", "Rusty ", "Rusted ", "Water ", "Hydrogen ", "Nanoparticle ", "Chemical ", "Reaction"
 ]
 
 function randMSG() {
